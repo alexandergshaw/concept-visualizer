8fd59f0bc7b1efcee9f96de73955aa565a2eda01
Alex Shaw <alexandergshaw@gmail.com>
2025-04-19 14:01:15 -0500

    updated next config

--- client-app/next.config.ts
+++ client-app/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/concept-visualizer', // Replace with your GitHub repo name
-  trailingSlash: true,
 };
 
 export default nextConfig;
