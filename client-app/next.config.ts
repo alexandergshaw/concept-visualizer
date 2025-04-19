d0954bb833cbfc8dd85e2d26f695aed94b8a1d7f
Alex Shaw <alexandergshaw@gmail.com>
2025-04-19 11:59:46 -0500

    added github pages support

--- client-app/next.config.ts
+++ client-app/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/your-repo-name', // Replace with your GitHub repo name
+  trailingSlash: true,
 };
 
 export default nextConfig;
